a2dfc0daf7f4bc5c0a0d072ce3ca52374f6943ab
Ronald Chan <the@gr8.ninja>
2024-12-30 19:35:09 +0800

    optimize dependencies

--- src/app.rs
+++ src/app.rs
@@ -14,12 +14,6 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="site-root" content={format!("{:?}", options.clone().site_root)}/>
-                <meta name="site-pkg-dir" content={format!("{:?}", options.clone().site_pkg_dir)}/>
-                <meta name="output-name" content={format!("{:?}", options.clone().output_name)}/>
-                <meta name="env" content={format!("{:?}", options.clone().env)}/>
-                <meta name="site-addr" content={format!("{:?}", options.clone().site_addr)}/>
-                <meta name="reload-port" content={format!("{:?}", options.clone().reload_port)}/>
                 <AutoReload options=options.clone() />
                 <HydrationScripts options/>
                 <MetaTags/>
